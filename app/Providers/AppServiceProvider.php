d1922c3965a39bd2d669962627d2989001788fd6
Higor de Deus <higordeus22@gmail.com>
2024-07-31 15:55:02 -0300

    Fix(http): Correção de http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Forçar HTTPS
-        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
     }
